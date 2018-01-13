55c297ba2237fbdc157005f1612d0cab8370b268
Chris Esposto <cesposto@gmail.com>
2018-01-12 23:16:51 -0800

    Updated based on comments

--- media.py
+++ media.py
@@ -3,7 +3,18 @@
 
 
 class Movie():
-    """The Class contains the title, trailer and poster about movies"""
+    """The Class contains the title, trailer and poster about movies
+
+    Args:
+        title (str): Person's firstname.
+        trailer_youtube_url (str): Person's lastname.
+        poster_image_url (str): Person's age.
+
+    Attributes:
+        title (str): Person's firstname.
+        trailer_youtube_url (str): Person's lastname.
+        poster_image_url (str): Person's age.
+    """
 
     def __init__(self, title, trailer_youtube_url, poster_image_url):
         self.title = title
